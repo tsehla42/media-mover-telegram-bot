90975aa2cb4e0f5439785ef2029a8a554369e2f3
Kyrylo <kirill.brezhnev02@gmail.com>
2025-09-07 13:59:02 +0300

    add throttling to single message handler

--- src/SingleMessageController.ts
+++ src/SingleMessageController.ts
@@ -1,17 +1,24 @@
-import { Api, Context } from "grammy";
+import { Context } from "grammy";
 import { EntityType, MediaEntity } from "./types";
 import { MY_ID, PHOTO_CHAT_ID, VIDEO_CHAT_ID } from "./config";
-import { extractMessagePropertiesFromContext, sendErrorLog, sendTextMessage } from "./utils";
+import { extractMessagePropertiesFromContext, sendErrorLog } from "./utils";
 import { PhotoSize, Video } from "@grammyjs/types/message";
-
-const ChatIdEntityTypeMap = {
-  photo: PHOTO_CHAT_ID,
-  video: VIDEO_CHAT_ID,
-} as const;
+import { Throttler } from "./Throttler";
 
 export class SingleMessageController {
+  static readonly ChatIdEntityTypeMap = {
+    photo: PHOTO_CHAT_ID,
+    video: VIDEO_CHAT_ID,
+  } as const;
+  static readonly idGetters = {
+    photo: (photo: PhotoSize[]) => photo[0].file_id,
+    video: (video: Video) => video.file_id,
+  } as const;
+
+  private readonly throttler = new Throttler(2000);
+
   public async processSingleMessage(ctx: Context, entityType: EntityType) {
-    const { entity, messageId } = extractMessagePropertiesFromContext(ctx, entityType)
+    const { entity, messageId } = extractMessagePropertiesFromContext(ctx, entityType);
     await this.sendSingleMessage(ctx, entity, entityType);
     await this.deleteMessage(ctx, messageId);
   }
@@ -26,22 +33,15 @@ export class SingleMessageController {
 
   private async sendSingleMessage(ctx: Context, entity: MediaEntity, entityType: EntityType) {
     const { api } = ctx;
-    const chatId = ChatIdEntityTypeMap[entityType];
-    const entityId = this[`${ entityType }IdGetter`](entity);
+    const chatId = SingleMessageController.ChatIdEntityTypeMap[entityType];
+    const entityId = SingleMessageController.idGetters[entityType](entity);
     const isPhoto = entityType === "photo";
     const isVideo = entityType === "video";
     try {
+      await this.throttler.throttle();
       return isPhoto ? api.sendPhoto(chatId, entityId) : isVideo ? api.sendVideo(chatId, entityId) : undefined;
     } catch (e) {
-      await sendErrorLog(ctx, `Error while sending ${entityType}`, e);
+      await sendErrorLog(ctx, `Error while sending ${ entityType }`, e);
     }
-  }
-
-  private photoIdGetter(photo: PhotoSize[]) {
-    return photo[0].file_id;
-  }
-
-  private videoIdGetter(video: Video) {
-    return video.file_id;
   }
 }
